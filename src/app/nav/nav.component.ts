ff83ddc33392a9ee3428545d1d39b6d497f01caa
Nhanth123 <honghan50@gmail.com>
2021-04-21 09:02:01 +0700

    code for nav

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { User } from '../_models/user';
 import { AccountService } from '../_services/account.service';
@@ -12,16 +13,15 @@ export class NavComponent implements OnInit {
   model: any = {};
   loggedIn: boolean;
 
-  constructor(public accountService: AccountService) {}
+  constructor(public accountService: AccountService, private router: Router) {}
 
   ngOnInit(): void {
     
   }
 
   login() {
-    this.accountService.login(this.model).subscribe(
-      (response) => {
-        console.log(response);
+    this.accountService.login(this.model).subscribe((response) => {
+        this.router.navigateByUrl('/members');
       },
       (error) => {
         console.log(error);
@@ -31,5 +31,6 @@ export class NavComponent implements OnInit {
 
   logout() {
     this.accountService.logout();
+    this.router.navigateByUrl('/');
   }
 }
