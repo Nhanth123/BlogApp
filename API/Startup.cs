b572aa6e088f66e8cb9731df400da89ba1443363
nhan.trinh <nhan.trinh@rochdalespears.com>
2021-07-17 02:01:09 +0700

    Done set up angular

--- API/Startup.cs
+++ API/Startup.cs
@@ -54,6 +54,7 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
             app.UseAuthorization();
 
